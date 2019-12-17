7feca34558bfc834c35b3a2c0efb5aed5643c067
Judd Guerrero <jguerrero@student.unimelb.edu.au>
2019-12-17 20:21:57 +1100

    Aggregating Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import MaterialTable from "material-table";
+import Container from '@material-ui/core/Container';
+import LineChart from './LineChart';
 
 
 const fetchTraffic = async (setData) =>{
@@ -8,8 +10,46 @@ const fetchTraffic = async (setData) =>{
     console.log(header);
     const body = await header.json();
     console.log(body);
-    setData(body.result.records);
+    await setData(body.result.records);
+
+    const resultsArray = body.result.records;
+
+    var reducedResult = resultsArray.map(function(item) {
+        return {dateCaptured: item.date_captured, volumeDay: item.volume_per_day};
+    });
+
+    var temporary = {};
+
+    reducedResult.forEach(function(d) {
+        if (temporary.hasOwnProperty(d.dateCaptured)) {
+            temporary[d.dateCaptured] = temporary[d.dateCaptured] + Number(d.volumeDay);
+        } else {
+            temporary[d.dateCaptured] = Number(d.volumeDay);
+        }
+    });
+
+    var aggregatedData = [];
+
+    for (var key in temporary) {
+        aggregatedData.push({ dateCaptured: key, volumeDay: temporary[key] });
+    }
 }
+
+const columns = [
+    {title: 'Volume/Day', field: 'volume_per_day'},
+    {title: 'Date Captured', field: 'date_captured'},
+    // {title: 'Direction 1', field: 'direction_1'},
+    // {title: 'Road Name', field: 'road_name'},
+    // {title: 'Direction 2', field: 'direction_2'},
+    // {title: 'Suburb', field: 'suburb'},
+    // {title: 'Full Text', field: '_full_text'},
+    // {title: 'Section Start', field: 'section_start'},
+    // {title: 'Section End', field: 'section_end'},
+    // {title: 'Vehicles 1', field: 'vehicles_1'},
+    // {title: 'Vehicles 2', field: 'vehicles_2'},
+    // {title: 'ID', field: '_id'},
+    // {title: '85th Percentile Speed', field: '85th_percentile_speed'},
+];
 
 function App() {
 
@@ -21,22 +61,16 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" style={{height: '100vh', width: '100vw'}}>
       <header className="App-header">
+          <Container maxWidth="lg">
         <MaterialTable
-            columns={[
-              { title: "Adı", field: "name" },
-              { title: "Soyadı", field: "surname" },
-              { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
-              {
-                title: "Doğum Yeri",
-                field: "birthCity",
-                lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-              }
-            ]}
+            columns={columns}
             data={data}
-            title="Demo Title"
+            title="Yarra Municipality Traffic Count"
         />
+        <LineChart/>
+          </Container>
       </header>
     </div>
   );
